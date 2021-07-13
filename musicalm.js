fcb8c4db1cb52b3c39ef2fd7b0861067665729b4
hungzero168 <47785168+hungzero168@users.noreply.github.com>
2021-07-13 16:39:38 +0700

    Update musicalm.js

--- musicalm.js
+++ musicalm.js
@@ -1,7 +1,3 @@
-http-response ^https:\/\/www\.peacefulsoundsapp\.com\/api\/v1\/init$ requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/langkhach270389/Scripting/master/musicalm.js,script-update-interval=0
-
-www.peacefulsoundsapp.com
-
 var obj = JSON.parse($response.body);
 
 obj["data"]["is_premium"]=1;
