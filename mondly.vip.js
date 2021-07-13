ae774970e5c1f3b2dd930981b4bf439bd1fb948e
hungzero168 <47785168+hungzero168@users.noreply.github.com>
2021-07-13 20:52:10 +0700

    Update mondly.vip.js

--- mondly.vip.js
+++ mondly.vip.js
@@ -1,10 +1,3 @@
-
-http-response ^https:\/\/api\.mondlylanguages\.com\/v1\/ios\/user\/sync$ requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/langkhach270389/Scripting/master/mondly.vip.js,script-update-interval=0
-
-api.mondlylanguages.com
-
-
-
 var obj = JSON.parse($response.body);
 
 obj["pucharses"]= [10];
