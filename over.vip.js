af2dad3a1f6fd0e2175804de16824ded1cfb3ab6
hungzero168 <47785168+hungzero168@users.noreply.github.com>
2021-07-13 20:52:31 +0700

    Update over.vip.js

--- over.vip.js
+++ over.vip.js
@@ -1,8 +1,3 @@
-http-response ^https:\/\/api\.overhq\.com\/(user\/token\/refresh$|subscription\/verifyReceipt$) requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/langkhach270389/Scripting/master/over.vip.js,script-update-interval=0
-
-api.overhq.com
-
-
 var body = $response.body;
 let obj = JSON.parse(body);
 obj.user.subscription["isSubscriptionActive"] = true;
