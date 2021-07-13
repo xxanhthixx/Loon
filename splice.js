3ad1c204076af8de51dab3fb6f3874c7c60b029b
hungzero168 <47785168+hungzero168@users.noreply.github.com>
2021-07-13 20:54:34 +0700

    Update splice.js

--- splice.js
+++ splice.js
@@ -1,8 +1,3 @@
-http-response https:\/\/splice\.oracle\.\w+\.com\/devices\/me requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/langkhach270389/Scripting/master/splice.js,script-update-interval=0
-
-splice.oracle.*.com
-
-
 var obj = JSON.parse($response.body); 
 obj['will_renew_subscription'] = true; 
 obj['is_subscribed'] = true; 
