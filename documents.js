60a013d684196610f31a8ca5e1378f41f3b2d55a
hungzero168 <47785168+hungzero168@users.noreply.github.com>
2021-07-13 20:51:32 +0700

    Update documents.js

--- documents.js
+++ documents.js
@@ -1,7 +1,3 @@
-http-response ^https:\/\/license\.pdfexpert\.com\/api\/.+\/subscription\/(refresh$|check$) requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/langkhach270389/Scripting/master/Pdfexpert.vip.js,script-update-interval=0
-
-^https:\/\/license\.pdfexpert\.com\/api\/.*\/(?:documents|pdfexpert6)\/subscription\/(refresh$|check$) requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/langkhach270389/Scripting/master/Pdfexpert.vip.js,script-update-interval=0
-
 var obj= {
   "originalTransactionId" : "20000625420102",
   "subscriptionState" : "trial",
