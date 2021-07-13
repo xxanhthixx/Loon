dca29e4093bc51f278304f82ccb78bb270a4811c
hungzero168 <47785168+hungzero168@users.noreply.github.com>
2021-07-13 20:50:08 +0700

    Update Faded.js

--- Faded.js
+++ Faded.js
@@ -1,7 +1,4 @@
 
-http-response ^https:\/\/api\.madewithfaded\.com\/api\/.+\/subscription\/validate$ requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/langkhach270389/Scripting/master/Faded.js,script-update-interval=0
-api.madewithfaded.com
-
 let obj= JSON.parse($response.body);
 obj= {
   "status": 200,
